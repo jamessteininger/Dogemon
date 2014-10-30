15ed74cf9bb778cce7410df64a3222f472907aca
ZiyiChen <chen216@mail.chapman.edu>
2014-10-30 03:34:13 +0000

    added log out at header

--- db/schema.rb
+++ db/schema.rb
@@ -73,9 +73,9 @@ ActiveRecord::Schema.define(version: 20141029001942) do
     t.integer  "experience"
     t.string   "name"
     t.string   "description"
-    t.string   "imageurl"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "imageurl"
   end
 
   create_table "equipment", force: true do |t|
