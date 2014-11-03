d0eb275cc8c06c70c9b68957a9848170ad92e933
neraken <ng125@mail.chapman.edu>
2014-11-03 09:24:27 +0000

    added default profile pictures on sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   
   validates :dogetag, presence: true
+  validates :imageurl, presence: true
   has_many :transactions
   has_many :sales, dependent: :destroy
   has_many :logs
