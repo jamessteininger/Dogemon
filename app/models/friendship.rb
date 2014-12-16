3bd32ebd3a6dec369ec1de174d1a10fc11c7d688
neraken <ng125@mail.chapman.edu>
2014-12-16 01:07:11 +0000

    added add friend button

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,3 +1,5 @@
 class Friendship < ActiveRecord::Base
   validates :user_id, presence: true validates :friend_id, presence: true, class_name: User
+  belongs_to :userbelongs_to: friend,
+belongs_to :friend, class_name: User
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20141215233537) do
+ActiveRecord::Schema.define(version: 20141215234654) do
 
   create_table "admin_users", force: true do |t|
     t.string   "email",                  default: "", null: false
