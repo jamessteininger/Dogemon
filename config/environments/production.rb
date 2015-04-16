708919a760cf341244f88ae731c5abf991fabb3d
James Steinigner <jesteininger@gmail.com>
2015-04-16 22:10:32 +0000

    Forgot password

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.action_mailer.default_url_options = { :host => 'projectdoge.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'godogemon.com' }
   #Rails.application.routes.default_url_options[:host] = 'projectdoge.herokuapp.com'
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
