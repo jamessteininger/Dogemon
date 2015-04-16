33af44b316db03108e447a53d099c62a6f4924e1
James Steinigner <jesteininger@gmail.com>
2015-04-16 06:37:51 +0000

    public pet profiles

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,6 +1,6 @@
 class PetsController < ApplicationController
   before_action :set_pet, only: [:show, :edit, :update, :destroy]
-before_filter :authenticate_user!
+  before_filter :authenticate_user!, only: [:edit, :uddate, :destroy, :challenge, :index]
   # GET /pets
   # GET /pets.json
   def index
